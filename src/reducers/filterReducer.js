e8f705594e135e32827a41d2e52d9acd5ade1bef
manimaran <moglimani@gmail.com>
2020-04-27 20:02:15 +0530

    initial setup

---
+++ src/reducers/filterReducer.js
@@ -0,0 +1,11 @@
+import * as types from '../actions';
+
+const initialState = {
+  filters: {}
+};
+export default function(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const HtmlWepackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const loader = require('sass-loader');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: '/.html$/',
+        use: {
+          loader: 'html-loader'
+        }
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      // {
+      //   test: /\.(jpg|png)$/,
+      //   use: {
+      //     loader: 'url-loader'
+      //   }
+      // },
+      {
+        test: /\.jpe?g$|\.ico$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.wav$|\.mp3$/,
+        // loader: 'images/file-loader?name=[name].[ext]' // <-- retain original file name
+        loader: 'file-loader',
+        options: {
+          name: 'assets/images/[name].[ext]'
+        }
+      }
+      // {
+      //   test: /\.svg$/,
+      //   use: ['@svgr/webpack', 'url-loader']
+      // }
+    ]
+  },
+  plugins: [
+    new Dotenv({
+      path: './.env-cmdrc', // Path to .env file (this is the default)
+      systemvars: true
+    }),
+    new HtmlWepackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    }),
+    new MiniCssExtractPlugin(),
+    new CopyPlugin([{ from: './assets', to: 'assets' }])
+  ]
+};
